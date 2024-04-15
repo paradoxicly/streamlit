378b1b752f76ba762e98b3593f0e51edd2ad1036
paradoxicly <166998529+paradoxicly@users.noreply.github.com>
2024-04-14 23:11:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
+import pandas as pd
 from pathlib import Path
 import streamlit as st
 
-for path in Path('.').iterdir():
-    st.write(path.name)
-  
+def list_files_in_directories():
+    cwd = Path.cwd()
+    directories = [cwd] + list(cwd.parents)
+    for directory in directories:
+        files = [f.name for f in directory.iterdir() if f.is_file()]
+        df = pd.DataFrame(files, columns=['Files'])
+        st.write(f'Files in {directory}:', df)
+
+list_files_in_directories()
